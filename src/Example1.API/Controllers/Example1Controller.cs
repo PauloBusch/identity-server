d55e72d533b1586b48a8e64021b7862b0752382d
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2020-06-06 15:08:51 -0300

    Nova api para testes

---
+++ src/Example1.API/Controllers/Example1Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Example1.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Example1Controller : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<dynamic> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new 
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55)
+            })
+            .ToArray();
+        }
+    }
+}
