1857bd77c4da2b5ca3d332c9ce0930df2f882e5d
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2020-09-07 13:15:26 -0300

    Context Factory

---
+++ Identity.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Identity.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<IdentityContext>
+    {
+        public IdentityContext CreateDbContext(string[] args)
+        {
+            // Utilizado para geração das migrações
+            var connectionString = "Server=localhost;Port=3306;Database=Identity;Uid=root;Pwd=123456";
+            var optionsBuilder = new DbContextOptionsBuilder<IdentityContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new IdentityContext(optionsBuilder.Options);
+        }
+    }
+}
