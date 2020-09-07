60ccf5dbee0c084061ec5c54a2366038319018a7
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2020-09-07 11:45:26 -0300

    Entities

---
+++ Identity.Domain/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using Identity.Domain.Utils.Common;
+using System;
+
+namespace Identity.Domain.Entities
+{
+    public class User : EntityBase
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string PasswordHash { get; private set; }
+
+        protected User() { }
+
+        public User(
+            Guid? id,
+            string name,
+            string email
+        ) : this()
+        {
+            CreatedAt = DateTime.UtcNow;
+            Id = id ?? Guid.NewGuid();
+            Name = name;
+            Email = email;
+        }
+
+        public void Change(
+            string name,
+            string email
+        ) {
+            UpdatedAt = DateTime.UtcNow;
+            Name = name;
+            Email = email;
+        }
+    }
+}
---
+++ Identity.Domain/Utils/Common/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Identity.Domain.Utils.Common
+{
+    public class EntityBase
+    {
+        public Guid Id { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+    }
+}
