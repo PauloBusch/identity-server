09cc3563b2885471bf87016f78077f4c0aa95a82
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2020-09-07 15:38:01 -0300

    Controller

---
+++ Identity.API/Controllers/IdentityControllerBase.cs
@@ -0,0 +1,30 @@
+﻿using Identity.Domain._Common.Results;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IdentityControllerBase : ControllerBase
+    {
+        protected Result GetResult(Result result) { 
+            Response.StatusCode = (int)result.Status;
+            return result;
+        }
+
+        protected Result<T> GetResult<T>(Result<T> result) where T : class
+        {
+            Response.StatusCode = (int) result.Status;
+            return result;
+        }
+
+        protected Result<T> GetResult<T>(T data) where T : class 
+        { 
+            return new Result<T>(data);
+        }
+    }
+}
---
+++ Identity.API/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using Identity.Domain._Common.Results;
+using Identity.Domain.Entities;
+using Identity.Domain.Interfaces.Services.Users;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Identity.API.Controllers
+{
+    public class UsersController : IdentityControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<Result<IEnumerable<User>>> ListAsync()
+        {
+            return GetResult(await _userService.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Result<User>> GetAsync([FromRoute] Guid id)
+        {
+            return GetResult(await _userService.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<Result> CreateAsync([FromBody] User user)
+        {
+            return GetResult(await _userService.CreateAsync(user));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<Result> UpdateAsync([FromRoute] Guid id, [FromBody] User user)
+        {
+            user.Id = id;
+            return GetResult(await _userService.UpdateAsync(user));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<Result> DeleteAsync([FromRoute] Guid id)
+        {
+            return GetResult(await _userService.DeleteAsync(id));
+        }
+    }
+}
